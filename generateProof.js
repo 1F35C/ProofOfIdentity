e517ca17ad9f1afa4e6f2e777e5ce8706d9799eb
1F35C <95659230+1F35C@users.noreply.github.com>
2021-12-15 21:16:20 -0500

    Updated generateProof for user input

--- generateProof.js
+++ generateProof.js
@@ -1,8 +1,13 @@
+PRIVATE_KEY_PATH = 'secrets/id_rsa';
+PUBLIC_KEY_PATH = 'secrets/id_rsa.pub';
+
 const openpgp = require('openpgp');
 const fs = require('fs');
 
-PRIVATE_KEY_PATH = 'secrets/id_rsa';
-PUBLIC_KEY_PATH = 'secrets/id_rsa.pub';
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 function generateMessage(name, date) {
   return 'Proof of 1F35C GitHub User Ownership\n' +
@@ -10,20 +15,27 @@ function generateMessage(name, date) {
          'Date:' + date.toUTCString();
 }
 
-// Sign 
-(async(message) => {
-  try {
+async function sign(message) {
     const privateKey = await openpgp.readPrivateKey({ armoredKey: fs.readFileSync(PRIVATE_KEY_PATH).toString() });
     const unsignedMessage = await openpgp.createCleartextMessage({ text: message });
     const cleartextMessage = await openpgp.sign({
         message: unsignedMessage,
         signingKeys: privateKey
     });
-    signed = cleartextMessage.toString();
-    console.log(signed);
-  } catch(err) {
-    console.error(err);
-  }
-})(generateMessage('ABC', new Date()));
+    return cleartextMessage.toString();
+}
 
+
+readline.question('Enter the name of organization:', org => {
+  let message = generateMessage(org, new Date());
+  (async() => {
+    try {
+      console.log(await sign(message));
+    } catch(err) {
+      console.error(err);
+    } finally {
+      readline.close();
+    }
+  })();
+});
 
